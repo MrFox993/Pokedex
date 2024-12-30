56177511eac4fa81587eed5b1411873846a93e8b
Marcel Fuchs <marcel.fuchs.1@gmx.de>
2024-12-30 14:11:58 +0100

    added types bg-color und types images

--- scripts/templates.js
+++ scripts/templates.js
@@ -6,9 +6,11 @@ function getCardsHTMLTemplate(pokemonIndex) {
                 <h5 class="card-title"># ${currentPokemons[pokemonIndex].id}</h5>
                 <h3 class="card-subtitle mb-2 text-body-secondary">${currentPokemons[pokemonIndex].name}</h3>
             </div>
-            <img src="${currentPokemons[pokemonIndex].image}" alt="${currentPokemons[pokemonIndex].name}">
-            <a href="#" class="card-link">Card link</a>
-            <a href="#" class="card-link">Another link</a>
+            <img class="${currentPokemons[pokemonIndex].types[0]}" src="${currentPokemons[pokemonIndex].image}" alt="${currentPokemons[pokemonIndex].name}">
+            <div class="card-footer">
+                <p>${currentPokemons[pokemonIndex].types[0]}</p>
+                <p>${currentPokemons[pokemonIndex].types[1]}</p>
+            </div>
         </div>
     </div>
     `;
