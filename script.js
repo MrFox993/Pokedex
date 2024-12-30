1875adef3b2ad724b53314744524c1d485062e80
Marcel Fuchs <marcel.fuchs.1@gmx.de>
2024-12-30 07:42:08 +0100

    display firstData

--- script.js
+++ script.js
@@ -5,18 +5,25 @@ let allPokemons = [];
 let currentPokemons = [];
 
 function init() {
-    currentPokemons = allPokemons;
     fetchFirstData();
+    currentPokemons = allPokemons;
 }
 
 async function fetchFirstData() {
     let response = await fetch(`${BASE_URL}?limit=${limit}&offset=${offset}`);
     let responseJson = await response.json();
-    let contentRef = document.getElementById('content');
     console.log(responseJson);
     let fetchResults = await responseJson.results;
 
     for (let index = 0; index < responseJson.results.length; index++) {
         await allPokemons.push(fetchResults[index]);
     }
+    renderPokemons();
 };
+
+async function renderPokemons() {
+    let contentRef = document.getElementById('content');
+    for (let pokemonIndex = 0; pokemonIndex < await currentPokemons.length; pokemonIndex++) {
+        contentRef.innerHTML += getCardsHTMLTemplate(pokemonIndex);
+    }
+}
