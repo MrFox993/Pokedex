b291fc55429fb2dda052f50c356a2e5bc05489ef
Marcel Fuchs <marcel.fuchs.1@gmx.de>
2024-12-30 10:55:22 +0100

    card-header adjusted

--- scripts/templates.js
+++ scripts/templates.js
@@ -1,13 +1,15 @@
 function getCardsHTMLTemplate(pokemonIndex) {
   return `
     <div class="card" style="width: 18rem;">
-    <div class="card-body flex column">
-      <h5 class="card-title"># ${currentPokemons[pokemonIndex].id}</h5>
-      <h6 class="card-subtitle mb-2 text-body-secondary">${currentPokemons[pokemonIndex].name}</h6>
-      <img src="${currentPokemons[pokemonIndex].image}" alt="${currentPokemons[pokemonIndex].name}">
-      <a href="#" class="card-link">Card link</a>
-      <a href="#" class="card-link">Another link</a>
+        <div class="card-body flex column">
+            <div class= "card-header">
+                <h5 class="card-title"># ${currentPokemons[pokemonIndex].id}</h5>
+                <h3 class="card-subtitle mb-2 text-body-secondary">${currentPokemons[pokemonIndex].name}</h3>
+            </div>
+            <img src="${currentPokemons[pokemonIndex].image}" alt="${currentPokemons[pokemonIndex].name}">
+            <a href="#" class="card-link">Card link</a>
+            <a href="#" class="card-link">Another link</a>
+        </div>
     </div>
-  </div>
     `;
 }
