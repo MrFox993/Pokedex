410d4753de2d5ab6f0f0bbff728916d63e79ca1d
Marcel Fuchs <marcel.fuchs.1@gmx.de>
2024-12-30 10:32:53 +0100

    render detail image

--- scripts/templates.js
+++ scripts/templates.js
@@ -1,10 +1,10 @@
 function getCardsHTMLTemplate(pokemonIndex) {
   return `
     <div class="card" style="width: 18rem;">
-    <div class="card-body">
+    <div class="card-body flex column">
       <h5 class="card-title"># ${currentPokemons[pokemonIndex].id}</h5>
       <h6 class="card-subtitle mb-2 text-body-secondary">${currentPokemons[pokemonIndex].name}</h6>
-      <img src="#" alt="#">
+      <img src="${currentPokemons[pokemonIndex].image}" alt="${currentPokemons[pokemonIndex].name}">
       <a href="#" class="card-link">Card link</a>
       <a href="#" class="card-link">Another link</a>
     </div>
