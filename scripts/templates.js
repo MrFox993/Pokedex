b36d6540175bd4d06a3f7a79b7e0ec52293390e4
Marcel Fuchs <marcel.fuchs.1@gmx.de>
2024-12-30 09:30:29 +0100

    styling adjustments

--- scripts/templates.js
+++ scripts/templates.js
@@ -2,7 +2,7 @@ function getCardsHTMLTemplate(pokemonIndex) {
   return `
     <div class="card" style="width: 18rem;">
     <div class="card-body">
-      <h5 class="card-title"># ${pokemonIndex}</h5>
+      <h5 class="card-title"># ${currentPokemons[pokemonIndex].id}</h5>
       <h6 class="card-subtitle mb-2 text-body-secondary">${currentPokemons[pokemonIndex].name}</h6>
       <img src="#" alt="#">
       <a href="#" class="card-link">Card link</a>
