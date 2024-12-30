ee72eb05f26a45881ab852e1450a80b01e11ed69
Marcel Fuchs <marcel.fuchs.1@gmx.de>
2024-12-30 10:32:37 +0100

    map detail information

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ async function init() {
 async function fetchFirstData() {
   let response = await fetch(`${BASE_URL}?limit=${limit}&offset=${offset}`);
   let responseJson = await response.json();
-  console.log(responseJson);
+//   console.log(responseJson);
   let fetchResults = await responseJson.results;
   return new Promise((resolve, reject) => {
     if (response.ok) {
@@ -23,12 +23,34 @@ async function fetchFirstData() {
   });
 }
 
+async function fetchPokemonData() {
+    return new Promise(async (resolve, reject) => {
+        try {
+            for (let pokemonIndex = 0; pokemonIndex < allFetchedPokemons.length; pokemonIndex++) {
+                let response = await fetch(`${allFetchedPokemons[pokemonIndex].url}`);
+                let responseJson = await response.json();
+                // console.log(`${allFetchedPokemons[pokemonIndex].name} data fetched:`, responseJson);
+                allFetchedPokemons[pokemonIndex] = {
+                    ...allFetchedPokemons[pokemonIndex],
+                    details: responseJson
+                };
+            }
+            resolve("data successfully fetched");
+        } catch (error) {
+            reject(`Error fetching Pokémon data: ${error}`);
+        }
+        
+    })
+}
+
 async function storeFetchedData() {
   try {
     let fetchedData = await fetchFirstData();
     allFetchedPokemons = fetchedData;
+    await fetchPokemonData();
     allFormattedPokemons = formatPokemonData(allFetchedPokemons);
     currentPokemons = allFormattedPokemons;
+    console.log(`allformattedPokemons: ` , allFormattedPokemons);
     renderPokemons();
   } catch (error) {
     console.error(error);
@@ -41,6 +63,9 @@ function formatPokemonData(allFetchedPokemons) {
             name: pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1),
             id: pokemon.url.split("/").filter(Boolean).pop(),
             url: pokemon.url,
+            details: pokemon.details,
+            types: pokemon.details.types.map(type => type.type.name),
+            image: pokemon.details.sprites.front_default
         };
     });
 }
