5141c646af7864574606be34540eb524df53fee1
Marcel Fuchs <marcel.fuchs.1@gmx.de>
2024-12-31 07:15:27 +0100

    adjusted styling

--- scripts/templates.js
+++ scripts/templates.js
@@ -6,12 +6,18 @@ function getCardsHTMLTemplate(pokemonIndex) {
                 <h5 class="card-title"># ${currentPokemons[pokemonIndex].id}</h5>
                 <h3 class="card-subtitle mb-2 text-body-secondary">${currentPokemons[pokemonIndex].name}</h3>
             </div>
-            <img class="${currentPokemons[pokemonIndex].types[0]}" src="${currentPokemons[pokemonIndex].image}" alt="${currentPokemons[pokemonIndex].name}">
-            <div class="card-footer">
+            <img class="${currentPokemons[pokemonIndex].types[0]}" src="${currentPokemons[pokemonIndex].image_default}" alt="${currentPokemons[pokemonIndex].name}">
+            <div id="card-types-${currentPokemons[pokemonIndex].id}" class="card-footer">
                 <p>${currentPokemons[pokemonIndex].types[0]}</p>
                 <p>${currentPokemons[pokemonIndex].types[1]}</p>
             </div>
         </div>
     </div>
+    `;
+}
+
+function getTypeHTMLTemplate(pokemonIndex, typeIndex) {
+    return `
+        <img class="${currentPokemons[pokemonIndex].types[typeIndex]}" src="./assets/img/${currentPokemons[pokemonIndex].types[typeIndex]}.png" alt="${currentPokemons[pokemonIndex].types[typeIndex]}">
     `;
 }
