6b5bc6ebd1b40e31431cd3b0a28b21557823ee8e
Marcel Fuchs <marcel.fuchs.1@gmx.de>
2025-01-03 08:06:19 +0100

    card background image implemented

--- scripts/templates.js
+++ scripts/templates.js
@@ -6,7 +6,7 @@ function getCardsHTMLTemplate(pokemonIndex) {
                 <h5 class="card-title"># ${currentPokemons[pokemonIndex].id}</h5>
                 <h3 class="card-subtitle mb-2 text-body-secondary">${currentPokemons[pokemonIndex].name}</h3>
             </div>
-            <img class="${currentPokemons[pokemonIndex].types[0]}" src="${currentPokemons[pokemonIndex].image_default}" alt="${currentPokemons[pokemonIndex].name}">
+            <img class="${currentPokemons[pokemonIndex].types[0]} pokeball-bgr" src="${currentPokemons[pokemonIndex].image_default}" alt="${currentPokemons[pokemonIndex].name}">
             <div id="card-types-${currentPokemons[pokemonIndex].id}" class="card-footer">
                 <p>${currentPokemons[pokemonIndex].types[0]}</p>
                 <p>${currentPokemons[pokemonIndex].types[1]}</p>
@@ -17,13 +17,13 @@ function getCardsHTMLTemplate(pokemonIndex) {
 }
 
 function getTypeHTMLTemplate(pokemonIndex, typeIndex) {
-    return `
+  return `
         <img class="${currentPokemons[pokemonIndex].types[typeIndex]}" src="./assets/img/${currentPokemons[pokemonIndex].types[typeIndex]}.png" alt="${currentPokemons[pokemonIndex].types[typeIndex]}">
     `;
 }
 
 function getLoadingSpinnerHTMLTemplate() {
-    return `
+  return `
         <svg class="spinner" width="300" height="300" xmlns="http://www.w3.org/2000/svg">
             <defs>
                 <linearGradient id="circle-fill" x1="0" y1="0" x2="0" y2="1">
@@ -40,7 +40,7 @@ function getLoadingSpinnerHTMLTemplate() {
 }
 
 function getLoadMoreButtonHTMLTemplate() {
-    return `
+  return `
         <button class="btn btn-secondary" type="button" onclick="storeMoreFetchedData()">Load more</button>
     `;
 }
