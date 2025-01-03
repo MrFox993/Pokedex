23ff9fdd6f65bb97378b5c26b5d0a49c466a4672
Marcel Fuchs <marcel.fuchs.1@gmx.de>
2025-01-03 09:54:26 +0100

    adjusted loading spinner

--- scripts/templates.js
+++ scripts/templates.js
@@ -22,20 +22,26 @@ function getTypeHTMLTemplate(pokemonIndex, typeIndex) {
     `;
 }
 
+// function getLoadingSpinnerHTMLTemplate() {
+//   return `
+//         <svg class="spinner" width="300" height="300" xmlns="http://www.w3.org/2000/svg">
+//             <defs>
+//                 <linearGradient id="circle-fill" x1="0" y1="0" x2="0" y2="1">
+//                     <stop offset="50%" stop-color="red" />
+//                     <stop offset="50%" stop-color="white" />
+//                 </linearGradient>
+//             </defs>
+//             <circle cx="150" cy="150" r="100" fill="url(#circle-fill)" stroke="black" stroke-width="4" />
+//             <circle cx="150" cy="150" r="30" fill="rgb(6, 36, 46)" stroke="black" stroke-width="4" />
+//             <line x1="60" y1="150" x2="120" y2="150" stroke="black" stroke-width="4" />
+//             <line x1="180" y1="150" x2="240" y2="150" stroke="black" stroke-width="4" />
+//         </svg>
+//     `;
+// }
+
 function getLoadingSpinnerHTMLTemplate() {
   return `
-        <svg class="spinner" width="300" height="300" xmlns="http://www.w3.org/2000/svg">
-            <defs>
-                <linearGradient id="circle-fill" x1="0" y1="0" x2="0" y2="1">
-                    <stop offset="50%" stop-color="red" />
-                    <stop offset="50%" stop-color="white" />
-                </linearGradient>
-            </defs>
-            <circle cx="150" cy="150" r="100" fill="url(#circle-fill)" stroke="black" stroke-width="4" />
-            <circle cx="150" cy="150" r="30" fill="rgb(6, 36, 46)" stroke="black" stroke-width="4" />
-            <line x1="60" y1="150" x2="120" y2="150" stroke="black" stroke-width="4" />
-            <line x1="180" y1="150" x2="240" y2="150" stroke="black" stroke-width="4" />
-        </svg>
+  <img src="./assets/img/pokeball.png" alt="" class="spinner">
     `;
 }
 
